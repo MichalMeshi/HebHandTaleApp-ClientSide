64665878a2197cba07beb0376a48a8d61c13368b
MichalMeshi <96175315+MichalMeshi@users.noreply.github.com>
2023-08-10 19:22:11 +0300

    connect to translate api by the flask server

--- app/src/main/java/com/example/test1/TranslateAPI.java
+++ app/src/main/java/com/example/test1/TranslateAPI.java
@@ -3,6 +3,7 @@ package com.example.test1;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -22,47 +23,56 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class TranslateAPI {
-    public void translateWord(String selectedWord) throws IOException, KeyManagementException, NoSuchAlgorithmException {
-        // Create a TrustManager that trusts all certificates
-        TrustManager[] trustAllCertificates = new TrustManager[]{
-                new X509TrustManager() {
-                    public X509Certificate[] getAcceptedIssuers() {
-                        return new X509Certificate[0];
-                    }
-                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
-                    }
-                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
-                    }
-                }
-        };
+    private static final String BASE_URL = "http:/192.168.68.110:5000";
 
-        // Create a SSL context with the custom TrustManager
-        SSLContext sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(null, trustAllCertificates, new java.security.SecureRandom());
+    public static String translateWord(String word, String language){
+        OkHttpClient client = new OkHttpClient();
 
-        // Set the custom SSL context in OkHttpClient
-        OkHttpClient client = new OkHttpClient.Builder()
-                .sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager)trustAllCertificates[0])
-                .build();
+        // Build the URL for the GET request
+        String url = BASE_URL + "/translate_word_with_google_api/" + word + "/" + language;
 
-        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
-        RequestBody body = RequestBody.create(mediaType, "q=Hello%2C%20world!&target=es&source=en");
         Request request = new Request.Builder()
-                .url("https://google-translate1.p.rapidapi.com/language/translate/v2")
-                .post(body)
-                .addHeader("content-type", "application/x-www-form-urlencoded")
-                .addHeader("Accept-Encoding", "application/gzip")
-                .addHeader("X-RapidAPI-Key", "cb66237962msh072cf932d6327eep1b0f92jsnc07647de39d9")
-                .addHeader("X-RapidAPI-Host", "google-translate1.p.rapidapi.com")
-                .addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36")
+                .url(url)
+                .get()
                 .build();
 
-        Response response = client.newCall(request).execute();
-        int statusCode = response.code();
-        String responseBody = response.body().string();
-        System.out.println("Status Code: " + statusCode);
-        System.out.println("Response Body: " + responseBody);
-//
-//        System.out.println(response);
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                String responseBody = response.body().string();
+                // The response seems to be a simple string, not a JSON object
+                System.out.println("Translation: " + responseBody);
+                return responseBody; // Return the response as is
+            } else {
+                System.out.println("Translation failed. Status code: " + response.code());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
+
+
+//    public void translateWord(String selectedWord) throws IOException, KeyManagementException, NoSuchAlgorithmException {
+//        // Set the custom SSL context in OkHttpClient
+//        OkHttpClient client = new OkHttpClient.Builder()
+//                .build();
+//        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+//        RequestBody body = RequestBody.create(mediaType, "q=Hello%2C%20world!&target=es&source=en");
+//        Request request = new Request.Builder()
+//                .url("https://google-translate1.p.rapidapi.com/language/translate/v2")
+//                .post(body)
+//                .addHeader("content-type", "application/x-www-form-urlencoded")
+//                .addHeader("Accept-Encoding", "application/gzip")
+//                .addHeader("X-RapidAPI-Key", "cb66237962msh072cf932d6327eep1b0f92jsnc07647de39d9")
+//                .addHeader("X-RapidAPI-Host", "google-translate1.p.rapidapi.com")
+//                .build();
+//
+//        Response response = client.newCall(request).execute();
+//        int statusCode = response.code();
+//        String responseBody = response.body().string();
+//        System.out.println("Status Code: " + statusCode);
+//        System.out.println("Response Body: " + responseBody);
+//    }
 }
