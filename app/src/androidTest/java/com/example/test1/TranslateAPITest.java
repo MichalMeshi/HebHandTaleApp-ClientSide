085b5c37110d4b795a4e04df2501b97b0d70bab0
MichalMeshi <96175315+MichalMeshi@users.noreply.github.com>
2023-08-13 13:58:18 +0300

    add TranslateAPITest to git

---
+++ app/src/androidTest/java/com/example/test1/TranslateAPITest.java
@@ -0,0 +1,22 @@
+package com.example.test1;
+
+import android.view.translation.Translator;
+
+import org.intellij.lang.annotations.Language;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class TranslateAPITest {
+    @Test
+    public void translate() throws IOException, NoSuchAlgorithmException, KeyManagementException {
+        TranslateAPI translate = new TranslateAPI();
+        translate.translateWord("שלום","en");
+    }
+}
